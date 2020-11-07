ba59710b5d5334feead619e55ffcbc0cd2f54abf
JeyM1 <denafen2000@gmail.com>
2020-11-07 19:41:54 +0200

    MORE MORE MORE Product tests.

---
+++ lab1_Pizza-creation/test/com/pizzamaker/Products/ProductTest.java
@@ -0,0 +1,23 @@
+package com.pizzamaker.Products;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ProductTest {
+    @Test
+    public void checkProductEquality() {
+        Pineapple pn = new Pineapple(100);
+        Pineapple pn2 = new Pineapple(100);
+
+        Tomato tom = new Tomato(100);
+
+        Assertions.assertEquals(pn, pn2);
+        Assertions.assertNotEquals((Product) pn, (Product) tom);
+
+        Cheese mozzarella = new Cheese(100, "mozzarella");
+        Cheese mozzarella2 = new Cheese(150, "mozzarella");
+
+        Assertions.assertNotEquals(mozzarella, mozzarella2);
+        Assertions.assertEquals(mozzarella, new Cheese(100, "mozzarella"));
+    }
+}
