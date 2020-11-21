4877e99776fa8d88614995a1575435b5724d21d1
JeyM1 <denafen2000@gmail.com>
2020-11-21 19:39:00 +0200

    [Tests/TomatoSauceTest] Refactored code and added more Mockito tests 2/2.

--- lab1_Pizza-creation/test/com/pizzamaker/PizzaTest.java
+++ lab1_Pizza-creation/test/com/pizzamaker/PizzaTest.java
@@ -23,7 +23,7 @@ public class PizzaTest {
         basicPizza = new Pizza(new ArrayList<>(), "basicPizza", PizzaSize.MEDIUM, basicIncompatibleProducts);
         try {
             basicPizza.addPizzaComponent(new Dough(100))
-                    //.addPizzaComponent(new Cheese(50, "mozzarella"))
+                    .addPizzaComponent(new Cheese(50, "mozzarella"))
                     .addPizzaComponent(new Mushrooms.Mushroom(5))
                     .addPizzaComponent(new Pepper(10));
         } catch (IncompatibleComponentException e) {
@@ -77,13 +77,15 @@ public class PizzaTest {
         Mockito.when(component.mass()).thenReturn(10);
         Mockito.when(component.count()).thenReturn(5);
         try {
-            basicPizza.addPizzaComponent(new Mushrooms(5));
+            basicPizza.addPizzaComponent(component);
         } catch (IncompatibleComponentException e) {
             e.printStackTrace();
         }
 
         Assertions.assertEquals(prevLength + 1, basicPizza.getComponents().size());
         Assertions.assertEquals(prevCount + 5, basicPizza.getActualComponentsCount());
+
+        Mockito.verify(component).count();
     }
 
     @Test
