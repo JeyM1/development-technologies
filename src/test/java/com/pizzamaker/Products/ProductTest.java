ced9f836ff21a507066739ba8aeb9380114987fa
JeyM1 <denafen2000@gmail.com>
2020-12-14 17:04:41 +0200

    [Project structure] Moved test to com.pizzamaker.

---
+++ src/test/java/com/pizzamaker/Products/ProductTest.java
@@ -0,0 +1,27 @@
+package com.pizzamaker.Products;
+
+import com.pizzamaker.Products.Cheese;
+import com.pizzamaker.Products.Pineapple;
+import com.pizzamaker.Products.Product;
+import com.pizzamaker.Products.Tomato;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ProductTest {
+    @Test
+    public void checkProductEquality() {
+        Pineapple pn = new Pineapple(100);
+        Pineapple pn2 = new Pineapple(100);
+
+        Tomato tom = new Tomato(100);
+
+        Assertions.assertEquals(pn, pn2);
+        Assertions.assertNotEquals((Product) pn, (Product) tom);
+
+        Cheese mozzarella = new Cheese(100, "mozzarella");
+        Cheese mozzarella2 = new Cheese(150, "mozzarella");
+
+        Assertions.assertNotEquals(mozzarella, mozzarella2);
+        Assertions.assertEquals(mozzarella, new Cheese(100, "mozzarella"));
+    }
+}
